c47dee0ced4edfaf60356f9cf85bfb1c4d72b54f
r_rony08 <robiul.rony1008@gmail.com>
2025-12-17 11:59:03 +0600

    feat(jobs): update and delete jobs with ownership rules

--- jobs/urls.py
+++ jobs/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import create_job, list_jobs
+from .views import create_job, list_jobs,update_job, delete_job
 
 urlpatterns = [
     path('', list_jobs),
     path('create/', create_job),
+    path('<int:job_id>/update/', update_job),
+    path('<int:job_id>/delete/', delete_job),
 ]
--- jobs/views.py
+++ jobs/views.py
@@ -1,11 +1,14 @@
 from rest_framework.decorators import api_view, permission_classes
-from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.permissions import IsAuthenticated,AllowAny
 from rest_framework.response import Response
 from rest_framework import status
+from django.shortcuts import get_object_or_404
 
 from .models import Job
 from .serializers import JobSerializer
+from .permissions import IsJobOwner
 from auth_app.permissions import IsRecruiter
+
 
 # Create Job Recruiter Only
 
@@ -30,3 +33,33 @@ def list_jobs(request):
     return Response(serializer.data)
 
 
+# Update Jobs
+
+@api_view(['PUT'])
+@permission_classes([IsAuthenticated, IsRecruiter, IsJobOwner])
+def update_job(request, job_id):
+    job = get_object_or_404(Job, id=job_id)
+
+    serializer = JobSerializer(
+        job,
+        data=request.data,
+        partial=True
+    )
+    serializer.is_valid(raise_exception=True)
+    serializer.save()
+
+    return Response(serializer.data)
+
+# Delete Jobs
+
+@api_view(['DELETE'])
+@permission_classes([IsAuthenticated, IsRecruiter, IsJobOwner])
+def delete_job(request, job_id):
+    job = get_object_or_404(Job, id=job_id)
+    job.is_active = False
+    job.save()
+
+    return Response(
+        {"message": "Job deactivated"},
+        status=status.HTTP_204_NO_CONTENT
+    )
